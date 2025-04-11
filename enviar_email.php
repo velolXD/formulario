d6a871a096cb3257159ce30a9610762ab5890b63
velolXD <juanarvila@gmail.com>
2025-04-11 02:25:21 -0500

    Update enviar_email.php

--- enviar_email.php
+++ enviar_email.php
@@ -1,6 +1,5 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Recibir datos del formulario
     $nombre_remitente = $_POST['nombre_remitente'];
     $email_remitente = $_POST['email_remitente'];
     $asunto = $_POST['asunto'];
@@ -8,20 +7,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $mensaje = $_POST['mensaje'];
 
     // Encabezados del correo
-    $headers = "MIME-Version: 1.0\r\n";
+    $headers  = "MIME-Version: 1.0\r\n";
     $headers .= "Content-type: text/html; charset=UTF-8\r\n";
-    $headers .= "From: " . $nombre_remitente . " <" . $email_remitente . ">\r\n";
-    $headers .= "Reply-To: " . $nombre_remitente . " <" . $email_remitente . ">\r\n";
+    $headers .= "From: $nombre_remitente <$email_remitente>\r\n";
+    $headers .= "Reply-To: $nombre_remitente <$email_remitente>\r\n";
 
-    // Envío del correo
+    // Enviar el correo
     if (mail($destinatario, $asunto, $mensaje, $headers)) {
         echo "El correo ha sido enviado correctamente.";
     } else {
         echo "Hubo un error al enviar el correo.";
     }
 } else {
-    // Si se accede directamente a este archivo sin enviar datos desde el formulario
     echo "Acceso no autorizado.";
 }
+?>
 
 
